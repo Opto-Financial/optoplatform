f39de6fac8edf5c56acb37fdf11450899a027eb4
Matthew Chang <mattchang908@gmail.com>
2023-01-11 13:17:42 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,20 @@ Rails.application.routes.draw do
   #------------------------------
 
   get("/", { :controller => "application", :action => "homepage" })
-  get("/home", { :controller => "application", :action => "homepage" })
-  get("/budgeting-and-planning", { :controller => "application", :action => "budgeting_and_planning" })
-  get("/profile", { :controller => "application", :action => "profile" })
-  get("/student-loans", { :controller => "application", :action => "student_loans" })
-
+  get '/home', to: 'application#homepage'
+  get '/budgeting-and-planning', to: 'application#budgeting_and_planning'
+  get '/profile', to: 'application#profile'
+  get '/student-loans', to: 'application#student_loans'
+  get '/sucuri-55d591a0065736ebad350201187df74f.php', to: 'application#sucuri_firewall'
+  
   # Routes for the Plaid item resource:
-
+  
   # CREATE ACCESS TOKEN
-  get("/create_plaid_link_token", { :controller => "plaid_items", :action => "create_link_token" })
-  post("/create_plaid_access_token", { :controller => "plaid_items", :action => "create_access_token" })
+  get '/create_plaid_link_token', to: 'plaid_items#create_link_token'
+  post '/create_plaid_access_token', to: 'plaid_items#create_access_token'
   
-  # CREATE
-  post("/insert_plaid_item", { :controller => "plaid_items", :action => "create" })
-          
-  # READ
-  get("/plaid_items", { :controller => "plaid_items", :action => "index" })
-  
-  get("/plaid_items/:path_id", { :controller => "plaid_items", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_plaid_item/:path_id", { :controller => "plaid_items", :action => "update" })
-  
-  # DELETE
-  get("/delete_plaid_item/:path_id", { :controller => "plaid_items", :action => "destroy" })
+  # CRUD plaid_item
+  resources :plaid_items, only: [:create, :index, :show, :update, :destroy]
 
   #------------------------------
 
@@ -146,189 +135,61 @@ Rails.application.routes.draw do
 
   # Routes for the Loan disbursement resource:
 
-  # CREATE
-  post("/insert_loan_disbursement", { :controller => "loan_disbursements", :action => "create" })
-          
-  # READ
-  get("/loan_disbursements", { :controller => "loan_disbursements", :action => "index" })
-  
-  get("/loan_disbursements/:path_id", { :controller => "loan_disbursements", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_loan_disbursement/:path_id", { :controller => "loan_disbursements", :action => "update" })
-  
-  # DELETE
-  get("/delete_loan_disbursement/:path_id", { :controller => "loan_disbursements", :action => "destroy" })
+  resources :loan_disbursements, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the School event resource:
 
-  # CREATE
-  post("/insert_school_event", { :controller => "school_events", :action => "create" })
-          
-  # READ
-  get("/school_events", { :controller => "school_events", :action => "index" })
-  
-  get("/school_events/:path_id", { :controller => "school_events", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_school_event/:path_id", { :controller => "school_events", :action => "update" })
-  
-  # DELETE
-  get("/delete_school_event/:path_id", { :controller => "school_events", :action => "destroy" })
+  resources :school_events, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Credit card resource:
 
-  # CREATE
-  post("/insert_credit_card", { :controller => "credit_cards", :action => "create" })
-          
-  # READ
-  get("/credit_cards", { :controller => "credit_cards", :action => "index" })
-  
-  get("/credit_cards/:path_id", { :controller => "credit_cards", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_credit_card/:path_id", { :controller => "credit_cards", :action => "update" })
-  
-  # DELETE
-  get("/delete_credit_card/:path_id", { :controller => "credit_cards", :action => "destroy" })
+  resources :credit_cards, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Loan comparison resource:
 
-  # CREATE
-  post("/insert_loan_comparison", { :controller => "loan_comparisons", :action => "create" })
-          
-  # READ
-  get("/loan_comparisons", { :controller => "loan_comparisons", :action => "index" })
-  
-  get("/loan_comparisons/:path_id", { :controller => "loan_comparisons", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_loan_comparison/:path_id", { :controller => "loan_comparisons", :action => "update" })
-  
-  # DELETE
-  get("/delete_loan_comparison/:path_id", { :controller => "loan_comparisons", :action => "destroy" })
+  resources :loan_comparisons, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Current loan resource:
 
-  # CREATE
-  post("/insert_current_loan", { :controller => "current_loans", :action => "create" })
-          
-  # READ
-  get("/current_loans", { :controller => "current_loans", :action => "index" })
-  
-  get("/current_loans/:path_id", { :controller => "current_loans", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_current_loan/:path_id", { :controller => "current_loans", :action => "update" })
-  
-  # DELETE
-  get("/delete_current_loan/:path_id", { :controller => "current_loans", :action => "destroy" })
+  resources :current_loans, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Budget expense resource:
 
-  # CREATE
-  post("/insert_budget_expense", { :controller => "budget_expenses", :action => "create" })
-          
-  # READ
-  get("/budget_expenses", { :controller => "budget_expenses", :action => "index" })
-  
-  get("/budget_expenses/:path_id", { :controller => "budget_expenses", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_budget_expense/:path_id", { :controller => "budget_expenses", :action => "update" })
-  
-  # DELETE
-  get("/delete_budget_expense/:path_id", { :controller => "budget_expenses", :action => "destroy" })
+  resources :budget_expenses, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the User Budget resource:
 
-  # CREATE
-  post("/insert_user_budget", { :controller => "user_budgets", :action => "create" })
-          
-  # READ
-  get("/user_budgets", { :controller => "user_budgets", :action => "index" })
-  
-  get("/user_budgets/:path_id", { :controller => "user_budgets", :action => "show" })
-  
-  # UPDATE
-  post("/modify_user_budget/:path_id", { :controller => "user_budgets", :action => "update" })
-  
-  # DELETE
-  get("/delete_user_budget/:path_id", { :controller => "user_budgets", :action => "destroy" })
+  resources :user_budgets, only: %i[create index show update destroy]
 
 #------------------------------
 
   # Routes for the Cash Flow resource:
 
-  # CREATE
-  post("/insert_cash_flow", { :controller => "cash_flows", :action => "create" })
-          
-  # READ
-  get("/cash_flows", { :controller => "cash_flows", :action => "index" })
-  
-  get("/cash_flows/:path_id", { :controller => "cash_flows", :action => "show" })
-  
-  # UPDATE
-  post("/modify_cash_flow/:path_id", { :controller => "cash_flows", :action => "update" })
-  
-  # DELETE
-  get("/delete_cash_flows/:path_id", { :controller => "cash_flows", :action => "destroy" })
+  resources :cash_flows, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Transaction resource:
 
-  # CREATE
-  post("/insert_transaction", { :controller => "transactions", :action => "create" })
-          
-  # READ
-  get("/transactions", { :controller => "transactions", :action => "index" })
-  
-  get("/transactions/:path_id", { :controller => "transactions", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_transaction/:path_id", { :controller => "transactions", :action => "update" })
-  
-  # DELETE
-  get("/delete_transaction/:path_id", { :controller => "transactions", :action => "destroy" })
+  resources :transactions, only: %i[create index show update destroy]
 
   #------------------------------
 
   # Routes for the Transaction category resource:
 
-  # CREATE
-  post("/insert_transaction_category", { :controller => "transaction_categories", :action => "create" })
-          
-  # READ
-  get("/transaction_categories", { :controller => "transaction_categories", :action => "index" })
-  
-  get("/transaction_categories/:path_id", { :controller => "transaction_categories", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_transaction_category/:path_id", { :controller => "transaction_categories", :action => "update" })
-  
-  # DELETE
-  get("/delete_transaction_category/:path_id", { :controller => "transaction_categories", :action => "destroy" })
+  resources :transaction_categories, only: %i[create index show update destroy]
 
   #------------------------------
 
