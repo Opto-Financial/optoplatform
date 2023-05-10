4987012ffe2c78e2b6ea42d58829eae23bf1f5ed
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-05-09 23:11:50 -0500

    removed unecessary tables, removed school stuff from screens

--- app/models/other_loan.rb
+++ app/models/other_loan.rb
@@ -18,9 +18,6 @@
 #  updated_at                    :datetime         not null
 #  user_id                       :integer
 #
-class CurrentLoan < ApplicationRecord
+class OtherLoan < ApplicationRecord
   belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id", :counter_cache => true })
-  has_many(:loan_disbursements, { :class_name => "LoanDisbursement", :foreign_key => "loan_id", :dependent => :destroy })
-
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,22 +11,15 @@
 #  budget_expenses_count        :integer
 #  budget_incomes_count         :integer
 #  cash_flows_count             :integer
-#  current_loans_count          :integer
 #  email                        :string
 #  first_name                   :string
 #  gender                       :string
-#  graduation_date              :date
-#  international_student_status :boolean
 #  last_name                    :string
-#  loan_comparisons_count       :integer
 #  password_digest              :string
 #  plaid_items_count            :integer
-#  premba_industry              :string
 #  reset_digest                 :string
 #  reset_sent_at                :datetime
-#  school                       :string
-#  school_events_count          :integer
-#  spend_intentions_count       :integer
+#  spender_profiles_count       :integer
 #  transaction_categories_count :integer
 #  user_budgets_count           :integer
 #  created_at                   :datetime         not null
@@ -41,14 +34,12 @@ class User < ApplicationRecord
   validates :email, :presence => true
   has_secure_password
 
-  has_many(:loan_comparisons, { :class_name => "LoanComparison", :foreign_key => "user_id", :dependent => :destroy })
-  has_many(:spend_intentions, { :class_name => "SpendIntention", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:spender_profiles, { :class_name => "SpenderProfile", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:accounts, { :class_name => "Account", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:budget_expenses, { :class_name => "BudgetExpense", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:budget_incomes, { :class_name => "BudgetIncome", :foreign_key => "user_id", :dependent => :destroy })
-  has_many(:current_loans, { :class_name => "CurrentLoan", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:other_loans, { :class_name => "OtherLoan", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:transaction_categories, { :class_name => "TransactionCategory", :foreign_key => "user_id", :dependent => :destroy })
-  has_many(:school_events, { :class_name => "SchoolEvent", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:plaid_items, { :class_name => "PlaidItem", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:user_budgets, { :class_name => "UserBudget", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:cash_flows, { :class_name => "CashFlow", :foreign_key => "user_id", :dependent => :destroy })
